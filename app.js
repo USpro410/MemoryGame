1ecc2fb2a88da5567f002b32a68dbd68b087ecbe
USpro410 <59206523+USpro410@users.noreply.github.com>
2020-08-16 23:12:13 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+// JavaScript source code
+document.addEventListener('DOMContentLoaded', () => {
+
+//card options
+const cardArray = [
+    {
+      name: 'January',
+      img: 'images/january.png'
+    },
+    {
+      name: 'February',
+      img: 'images/february.png'
+    },
+    {
+      name: 'March',
+      img: 'images/march.png'
+    },
+    {
+      name: 'April',
+      img: 'images/april.png'
+    },
+    {
+      name: 'May',
+      img: 'images/may.png'
+    },
+    {
+      name: 'June',
+      img: 'images/june.png'
+    },
+    {
+      name: 'July',
+      img: 'images/July.png'
+    },
+    {
+      name: 'August',
+      img: 'images/august.png'
+    },
+    {
+      name: 'September',
+      img: 'images/september.png'
+    },
+    {
+      name: 'October',
+      img: 'images/october.png'
+    },
+    {
+      name: 'November',
+      img: 'images/november.png'
+    },
+    {
+      name: 'December',
+      img: 'images/december.png'
+    },
+    {
+      name: 'January',
+      img: 'images/january.png'
+    },
+    {
+      name: 'February',
+      img: 'images/february.png'
+    },
+    {
+      name: 'March',
+      img: 'images/march.png'
+    },
+    {
+      name: 'April',
+      img: 'images/april.png'
+    },
+    {
+      name: 'May',
+      img: 'images/may.png'
+    },
+    {
+      name: 'June',
+      img: 'images/june.png'
+    },
+    {
+      name: 'July',
+      img: 'images/July.png'
+    },
+    {
+      name: 'August',
+      img: 'images/august.png'
+    },
+    {
+      name: 'September',
+      img: 'images/september.png'
+    },
+    {
+      name: 'October',
+      img: 'images/october.png'
+    },
+    {
+      name: 'November',
+      img: 'images/november.png'
+    },
+    {
+      name: 'December',
+      img: 'images/december.png'
+    }
+  ]
+
+  cardArray.sort(() => 0.5 - Math.random())
+const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+var cardsChosen = []
+var cardsChosenId = []
+const cardsWon = []
+//create your board
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img')
+      card.setAttribute('src', 'images/blank.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+}
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    
+    if(optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('You clicked the same image!')
+    }
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match!')
+      cards[optionOneId].setAttribute('src', 'images/white.png')
+      cards[optionTwoId].setAttribute('src', 'images/white.png')
+      cards[optionOneId].removeEventListener('click', flipCard)
+      cards[optionTwoId].removeEventListener('click', flipCard)
+      cardsWon.push(cardsChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
+      resultDisplay.textContent = 'Congratulations! You found them all! Refresh to play again!'
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length ===2) {
+      setTimeout(checkForMatch, 500)
+    }
+  }
+
+  createBoard()
+})
